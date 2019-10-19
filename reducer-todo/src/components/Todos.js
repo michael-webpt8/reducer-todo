2dc72daeb0816f3cae94d2f8f90ca4f1a48eaf4e
Michael Williams <manicdesigner@gmail.com>
2019-10-19 12:15:44 -0400

    add todo listing

---
+++ reducer-todo/src/components/Todos.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Todo from './Todo';
+
+function Todos(props) {
+  return (
+    <>
+      {/* {props.todoList.map(item => {
+        return <Todo key={item.id} item={state} />;
+      })} */}
+    </>
+  );
+}
+export default Todos;
