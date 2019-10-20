a8da2dabc2c55ac62bac82e15cd864b0875f19ff
Michael Williams <manicdesigner@gmail.com>
2019-10-20 12:40:52 -0400

    reformatted to use createTodo rework handleChange to e.target.value submieHandler => accepts createTodo(todo) useState

--- reducer-todo/src/components/TodoForm.js
+++ reducer-todo/src/components/TodoForm.js
@@ -1,29 +1,34 @@
 import React, { useState } from 'react';
 
-function TodoForm() {
-	const [todo, setTodo] = useState();
-	const [todos, setTodos] = useState();
+function TodoForm(props) {
+  const { createTodo } = props;
+  const [todo, setTodo] = useState('');
 
-	const handleChange = () => {
-		setTodo(todo);
-	};
+  const handleChange = e => {
+    setTodo(e.target.value);
+  };
 
-	const submithandler = (e) => {
-		e.preventDefault();
-	};
-	return (
-		<>
-			<form onSubmit={submithandler}>
-				<input
-					type="text"
-					onChange={handleChange}
-					value={todo}
-					placeholder="Add todo..."
-				/>
-				<br />
-				<button type="submit">Add Todo</button>
-			</form>
-		</>
-	);
+  const submithandler = e => {
+    e.preventDefault();
+    if (todo) {
+      createTodo(todo);
+    }
+    console.log(todo);
+    setTodo('');
+  };
+  return (
+    <>
+      <form onSubmit={submithandler}>
+        <input
+          type='text'
+          onChange={handleChange}
+          value={todo}
+          placeholder='Add todo...'
+        />
+        <br />
+        <button type='submit'>Add Todo</button>
+      </form>
+    </>
+  );
 }
 export default TodoForm;
