a3546de8546de45ea101f69b8853ade59518d1d0
Michael Williams <manicdesigner@gmail.com>
2019-10-20 19:11:09 -0400

    working on ways to get TOGGLE_COMPLETE to work

--- reducer-todo/src/reducers/todo.js
+++ reducer-todo/src/reducers/todo.js
@@ -12,10 +12,26 @@ export function reducer(state, action) {
         id: Date.now(),
         completed: false
       });
-    case 'TOGGLE_COMPLETE':
+    // case 'TOGGLE_COMPLETE':
+    //   return state.map(todo => {
+    //     if (todo.id === action.id) {
+    //       return { ...todo, completed: !todo.completed };
+    //     } else {
+    //       return todo;
+    //     }
+    //   });
+    case 'TODO_COMPLETE':
       return state.map(todo => {
         if (todo.id === action.id) {
-          return { ...todo, completed: !todo.completed };
+          return { ...todo, completed: true };
+        } else {
+          return todo;
+        }
+      });
+    case 'TODO_INCOMPLETE':
+      return state.map(todo => {
+        if (todo.id === action.id) {
+          return { ...todo, completed: false };
         } else {
           return todo;
         }
